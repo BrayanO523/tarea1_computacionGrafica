e13aa73ad30645d47d99c9bd0071b668f2c1b471
brayan-jose-ortiz <134749280+BrayanO523@users.noreply.github.com>
2024-10-24 21:07:05 -0600

    update
    ...

---
+++ app/src/main/java/com/example/reconocimientoocr/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.reconocimientoocr.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
